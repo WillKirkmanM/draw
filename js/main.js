bb9b37831c54de563a9b3ac433900ba29c5f6427
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-05-16 10:11:38 +0100

    Feat(main.js): Initialise Dark Mode on Load

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import { DrawingApp } from './DrawingApp.js';
+import { initDarkMode } from './darkMode.js';
+import "./sampleShapes.js"
 
 // Initiaise the app when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   const app = DrawingApp.getInstance();
   app.init();
   window.drawingApp = app;
+
+  initDarkMode();
 });
